903250e3d59dc3947cfd281f5d6271dfd8f90d1d
Alex Ledger <alex@playgroundlabs.io>
2025-03-29 10:16:53 -0700

    Set up hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
 </script>
 
-<section>
-	<h1>Alex Ledger</h1>
-	<p class="landing-page-text">
-		I'm an educator and web developer from Portland, Oregon. I work with businesses to design and
-		build great websites. I'm currently accepting new clients.
-	</p>
+<section class="hero">
+	<h1 class="font-decorative size-step-7">Alex Ledger</h1>
+	<div class="size-step-2 mt-2xs">
+		<p class="mt-0">
+			I'm a web designer and developer from Portland, Oregon. I work with businesses to design and
+			build websites.
+		</p>
+		<p class="mt-m">
+			I specialize in creating beautiful, functional websites that help businesses grow. I focus on
+			combining good fundamentals--strong, clear copy and design--with a little bit of magic to make
+			the experience fun and attention-grabbing.
+		</p>
+	</div>
 </section>
+<section>
+	<h2>Services</h2>
+</section>
+
+<style>
+	.hero p {
+		max-width: 40ch;
+	}
+</style>
