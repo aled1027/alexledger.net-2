e3124792974f7672964364503f6f7250ce776e58
Alex Ledger <alex@playgroundlabs.io>
2025-03-31 09:45:07 -0700

    Updates

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,45 +2,36 @@
 </script>
 
 <section class="about">
-	<h1 class="font-decorative size-step-7">Alex Ledger</h1>
-	<div>
-		<p>
-			I'm a web designer and developer from Portland, Oregon. I work with businesses to design and
-			build websites.
-		</p>
-		<p>
-			I have worked in software development for over 10 years. In that time, I've worked with great
-			institutions like the Food and Drug Administration, the Department of Homeland Security, MIT
-			Lincoln Laboratory, IBM Watson Health, Point72, and Bank of New York Mellon.
-		</p>
-		<p>
-			Over the years, I've worked on a wide range of projects, from small, local businesses to
-			large, international organizations. My contributions have ranged from integrating complex data
-			systems to building out the frontend consumer-facing applications.
-		</p>
-		<p>
-			I specialize in creating beautiful, functional websites that help businesses grow. I focus on
-			combining good fundamentals— strong, clear copy and design— with a sprinkle of magic to make
-			the experience fun and attention-grabbing.
-		</p>
-		<p>
-			You can read my professional <a href="#">newsletter</a>, check out my
-			<a href="https://alexledger.substack.com">personal substack</a>, or follow me on
-			<a href="https://bsky.app/profile/alexledger.bsky.social">bluesky</a>.
-		</p>
-	</div>
+	<h1>Alex Ledger</h1>
+	<p class="mt-half-em">
+		I'm a web designer and developer from Portland, Oregon. I work with businesses to design and
+		build websites.
+	</p>
+	<p>
+		I have worked in software development for over 10 years. In that time, I've worked with great
+		institutions like the Food and Drug Administration, the Department of Homeland Security, MIT
+		Lincoln Laboratory, IBM Watson Health, Point72, and Bank of New York Mellon.
+	</p>
+	<p>
+		Over the years, I've worked on a wide range of projects, from small, local businesses to large,
+		international organizations. My contributions have ranged from integrating complex data systems
+		to building out the frontend consumer-facing applications.
+	</p>
+	<p>
+		I specialize in creating beautiful, functional websites that help businesses grow. I focus on
+		combining good fundamentals— strong, clear copy and design— with a sprinkle of magic to make the
+		experience fun and attention-grabbing.
+	</p>
+	<p>
+		You can read my professional <a href="#">newsletter</a>, check out my
+		<a href="https://alexledger.substack.com">personal substack</a>, or follow me on
+		<a href="https://bsky.app/profile/alexledger.bsky.social">bluesky</a>.
+	</p>
 </section>
 
 <style>
-	.about p {
-		margin-block-start: 1em;
-		max-width: 60ch;
-		line-height: 1.5;
-		text-wrap: pretty;
-		font-size: var(--size-step-0);
-	}
-
-	.about p:first-child {
-		margin-block-start: 0.5em;
+	.about {
+		max-width: var(--content-width-narrow);
+		margin-inline: auto;
 	}
 </style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+</script>
+
+<section class="contact">
+	<h1 class="font-decorative">Contact</h1>
+	<p class="mt-half-em">I'd love to chat with you about how I can help you grow your business.</p>
+	<p>
+		You can book an initial call by filling out the form below or email me: hello@alexledger.net.
+	</p>
+	<form class="mt-l">
+		<div class="input-group">
+			<label for="name">Name</label>
+			<input type="text" id="name" name="name" />
+		</div>
+		<div class="input-group">
+			<label for="email">Email</label>
+			<input type="email" id="email" name="email" />
+		</div>
+		<div class="input-group">
+			<label for="message">Message</label>
+			<textarea id="message" name="message"></textarea>
+		</div>
+		<button class="button mt-m" type="submit">Submit</button>
+	</form>
+</section>
+
+<style>
+	.contact {
+		max-width: var(--content-width-narrow);
+		margin-inline: auto;
+	}
+
+	.contact button {
+		/* Playing with fibonacci numbers for button size */
+		padding-inline: 34px;
+		padding-block: 21px;
+
+		padding-inline: 21px;
+		padding-block: 13px;
+		font-size: var(--size-step-1);
+		font-weight: var(--font-semibold);
+	}
+
+	.contact .input-group {
+		display: flex;
+		flex-direction: column;
+		gap: var(--space-2xs);
+	}
+</style>
